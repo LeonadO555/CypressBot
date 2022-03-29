8bbc7ae5f57dece2f5020c5bed2a02506831291c
Leonid Mihaylov <leonmixt@gmail.com>
2022-03-30 00:08:34 +0300

    edit path

--- tgBot/db.js
+++ tgBot/db.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
-let dataActualUser = fs.readFileSync('MacintoshHD/Users/leo/Downloads/dojob-main/tmp/dataActualUser.json', 'utf8');
-let dataAllUsers = fs.readFileSync('MacintoshHD/Users/leo/Downloads/dojob-main/tmp/dataAllUser.json', 'utf8');
+let dataActualUser = fs.readFileSync('tmp/dataActualUser.json', 'utf8');
+let dataAllUsers = fs.readFileSync('tmp/dataAllUser.json', 'utf8');
 export function getActualUser() {
   return new Promise((resolve) => {
     setTimeout(() => {
