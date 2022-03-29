467981b6736c1349774e2967d762366cfd2bcf87
Leonid Mihaylov <leonmixt@gmail.com>
2022-03-30 00:38:14 +0300

    edit path

--- tgBot/app.js
+++ tgBot/app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+const express = require('express');
 import { PORT, TOKEN } from './config.js';
 import { Telegraf } from 'telegraf';
 import { getMainMenu } from './keyboards.js';
