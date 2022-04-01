8b8016fcc5cbb978fdffab3c76dcdbdfde29ceab
Leonid Mihaylov <leonmixt@gmail.com>
2022-04-02 00:12:43 +0300

    special error

--- cypress/common/commonOnMars.js
+++ cypress/common/commonOnMars.js
@@ -18,7 +18,7 @@ const linkToJob = () => {
 };
 
 const loginUser = (userName, userPassword) => {
-  cy.contains('Login').should('be.visible').click();
+  // cy.contains('Login').should('be.visible').click();
   cy.get('input[name="email"]').should('be.visible').type(userName).should('have.value', userName);
   cy.get('input[name="password"]').should('be.visible').type(userPassword).should('have.value', userPassword);
   cy.get('button[type="submit"]').should('be.visible').click();
