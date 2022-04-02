5ebd4f94342ea62672da77ba8ef4fe552235afb1
Leonid Mihaylov <leonmixt@gmail.com>
2022-04-03 01:30:28 +0300

    add wait in CompleteJob

--- tgBot/app.js
+++ tgBot/app.js
@@ -11,7 +11,7 @@ function getMainMenu() {
 }
 
 let dataActualUser = fs.readFileSync('/tmp/dataActualUser.json', 'utf8');
-let dataLog = fs.readFileSync('/tmp/dataAllUser.json', 'utf8');
+let dataLog = fs.readFileSync('$JENKINS_HOME/workspace/path/to/job/console.log', 'utf8');
 function getAllUsers() {
   return new Promise((resolve) => {
     setTimeout(() => {
