d1d1f3fd153bc13aa1cc1bcf62860dd06aa06e12
Leonid Mihaylov <leonmixt@gmail.com>
2022-04-03 01:36:50 +0300

    add wait in CompleteJob

--- tgBot/app.js
+++ tgBot/app.js
@@ -11,10 +11,7 @@ function getMainMenu() {
 }
 
 let dataActualUser = fs.readFileSync('/tmp/dataActualUser.json', 'utf8');
-let dataLog = fs.readFileSync(
-  'http://e7290ed965b5.sn.mynetname.net:8888/job/runme/workspace/path/to/job/console.log',
-  'utf8'
-);
+let dataLog = fs.readFileSync('http://e7290ed965b5.sn.mynetname.net:8888/job/runme/76/consoleText', 'utf8');
 function getAllUsers() {
   return new Promise((resolve) => {
     setTimeout(() => {
