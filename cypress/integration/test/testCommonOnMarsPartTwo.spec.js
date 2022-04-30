e454e131bd1ce99ef2adf2af89bec445ef586f8b
Leonid Mihaylov <leonmixt@gmail.com>
2022-05-01 00:47:03 +0300

    edit common onmars

--- cypress/integration/test/testCommonOnMarsPartTwo.spec.js
+++ cypress/integration/test/testCommonOnMarsPartTwo.spec.js
@@ -9,8 +9,6 @@ before(function () {
 describe('DO JOB', () => {
   it('Click Do Job button and wait 1 minute', () => {
     const arr = [];
-    onMarsCommon(Cypress.env('user_9'), Cypress.env('pas_9'), arr, 'USERNAME 9');
-    cy.writeFile('/tmp/dataAllUser.json', arr);
     onMarsCommon(Cypress.env('user_10'), Cypress.env('pas_10'), arr, 'USERNAME 10');
     cy.writeFile('/tmp/dataAllUser.json', arr);
     onMarsCommon(Cypress.env('user_11'), Cypress.env('pas_11'), arr, 'USERNAME 11');
@@ -24,6 +22,8 @@ describe('DO JOB', () => {
     onMarsCommon(Cypress.env('user_15'), Cypress.env('pas_15'), arr, 'USERNAME 15');
     cy.writeFile('/tmp/dataAllUser.json', arr);
     onMarsCommon(Cypress.env('user_16'), Cypress.env('pas_16'), arr, 'USERNAME 16');
+    cy.writeFile('/tmp/dataAllUser.json', arr);
+    onMarsCommon(Cypress.env('user_17'), Cypress.env('pas_17'), arr, 'USERNAME 17');
     cy.writeFile('/tmp/dataAllUser.json', arr);
   });
 });
