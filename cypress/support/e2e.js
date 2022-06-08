0346f62213016c90443142fc39cbe44ab7abbbec
Leonid Mihaylov <leonmixt@gmail.com>
2022-06-09 00:38:43 +0300

    edit common onmars

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,6 +1,6 @@
 // ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
+// This example support/e2e.js is processed and
+// loaded automatically before your e2e files.
 //
 // This is a great place to put global configuration and
 // behavior that modifies Cypress.
