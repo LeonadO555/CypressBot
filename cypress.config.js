01457edff3836204ea93803bcee602fab3d47e4c
Leonid Mihaylov <leonmixt@gmail.com>
2022-06-08 22:31:56 +0300

    edit common onmars

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,17 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    waitForAnimations: true,
-    viewportWidth: 1400,
-    viewportHeight: 800,
-    pageLoadTimeout: 140000,
-    taskTimeout: 30000,
-    redirectionLimit: 50,
-    defaultCommandTimeout: 30000,
-    screenshotOnRunFailure: false,
-    video: false,
-    retries: { runMode: 0, openMode: 0 },
-  },
+  waitForAnimations: true,
+  viewportWidth: 1400,
+  viewportHeight: 800,
+  pageLoadTimeout: 140000,
+  taskTimeout: 30000,
+  redirectionLimit: 50,
+  defaultCommandTimeout: 30000,
+  screenshotOnRunFailure: false,
+  video: false,
+  retries: { runMode: 0, openMode: 0 },
+  // component: {
+  //   supportFile: 'cypress/support/component.js',
+  // },
 });
